4526f9ecc8a3d8eb76a58ffa7262a7424c230767
Artur0705 <artlil420@gmail.com>
2022-06-25 22:51:42 +1000

    Created function for local storage and distinguished the past present and future

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,45 @@
 // Showing current date using moment
+var workHours = [
+  "09am",
+  "10am",
+  "11am",
+  "12pm",
+  "01pm",
+  "02pm",
+  "03pm",
+  "04pm",
+  "05pm",
+];
+var today = moment()
+var hourFormat = "hha";
+var currentHour = today.format(hourFormat);
+var currentDay = today.format("dddd, MMMM Do");
 
-var currentDay = moment().format("dddd, MMMM Do")
-$("#currentDay").text(currentDay) 
+$("#currentDay").text(currentDay);
+
+for (var i = 0; i <= workHours.length; i++) {
+  var hour = workHours[i];
+  var hourValue = localStorage.getItem(hour);
+  var textarea = $("#" + hour + "-textarea");
+
+  if (hourValue) {
+    textarea.val(hourValue);
+  }
+
+  var futureWorkHours = workHours.slice(workHours.indexOf(currentHour) + 1, workHours.length)
+  var isFuture = futureWorkHours.includes(hour)
+  var isPresent = hour === currentHour;
+  
+  if (isFuture) {
+    textarea.attr("class", "future");
+  } else if (isPresent) {
+    textarea.attr("class", "present");
+  } else {
+    textarea.attr("class", "past");
+  }
+}
+
+function onSave(hour) {
+  var hourValue = $("#" + hour + "-textarea").val();
+  localStorage.setItem(hour, hourValue);
+}
